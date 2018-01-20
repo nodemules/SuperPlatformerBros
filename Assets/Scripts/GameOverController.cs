4a78dd87a8f79198614fef51f0acfb3721a3b28e
brenthaertlein <brenthaertlein@gmail.com>
2018-01-20 01:45:08 -0500

    2D Side Scroller (Super Platformer Bros) Episode 2.5 - Misc improvements
    * unversioned files missed in last commit
    * save scenes

---
+++ Assets/Scripts/GameOverController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour
+{
+    private int _ending;
+
+    private GameObject _endingText;
+
+    public void Start()
+    {
+        GameObject gameOverCamera = GameObject.Find("GameOverCamera");
+        Camera camera = gameOverCamera.GetComponent<Camera>();
+        camera.enabled = true;
+        _ending = ApplicationState.Ending;
+        RunEnding();
+    }
+
+    private void RunEnding()
+    {
+        switch (_ending)
+        {
+            case -1:
+                _endingText = GameObject.Find("YouDeadText");
+                break;
+            case 1:
+                _endingText = GameObject.Find("YouWinText");
+                break;
+            default:
+//                RestartGame();
+                break;
+        }
+
+        if (_endingText == null)
+        {
+//            RestartGame();
+            return;
+        }
+
+
+        _endingText.SetActive(true);
+    }
+
+    private static void RestartGame()
+    {
+        ApplicationState.Ending = 1;
+        SceneManager.LoadScene("Main");
+    }
+}
