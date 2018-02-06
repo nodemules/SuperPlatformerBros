c10fd3aa4eef2c0de94cd291a972b07038cdff29
staticsound <johngreedjr@gmail.com>
2018-02-05 22:45:06 -0500

    2D Side Scroller (Super Platformer Bros) Episode 2 - Camera & Death
    * Make coin in UI spin because why not

--- Assets/Scripts/UICoinController.cs
+++ Assets/Scripts/UICoinController.cs
@@ -1,20 +1,22 @@
-﻿using System.Runtime.InteropServices;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class UICoinController : MonoBehaviour
 {
     private Text _coinText;
+    private GameObject _coinIcon;
     private static int _coinCount;
-
+   
     public void Start()
     {
         _coinText = GetComponentInChildren<Text>();
+        _coinIcon = GameObject.Find("UICoinIcon");
     }
 
     private void Update()
-    {
+    {     
         _coinText.text = "x " + _coinCount;
+        _coinIcon.transform.Rotate(new Vector2(0.0f, 45) * Time.deltaTime);
     }
 
     public static void CollectCoin()
