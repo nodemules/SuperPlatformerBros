87ebf7f7393450d3b7102ecc04805b699768e6f7
brenthaertlein <brenthaertlein@gmail.com>
2018-02-09 23:45:16 -0500

    2D Side Scroller (Super Platformer Bros) Episode 2.75 - Collectables
    * CollectCoinEffect

--- Assets/Scripts/UICoinController.cs
+++ Assets/Scripts/UICoinController.cs
@@ -1,20 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Linq.Expressions;
+using Environment;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class UICoinController : MonoBehaviour
 {
+    private static int _defaultFontSize = 18;
     private Text _coinText;
     private GameObject _coinIcon;
+    private int _oldCoinCount;
     private static int _coinCount;
-   
+
     public void Start()
     {
         _coinText = GetComponentInChildren<Text>();
         _coinIcon = GameObject.Find("UICoinIcon");
+        _defaultFontSize = _coinText.fontSize;
     }
 
     private void Update()
-    {     
+    {
+        if (_oldCoinCount < _coinCount)
+        {
+            CollectCoinEffect();
+        }
+
+        _oldCoinCount = _coinCount;
         _coinText.text = "x " + _coinCount;
         _coinIcon.transform.Rotate(new Vector2(0.0f, 45) * Time.deltaTime);
     }
@@ -23,4 +34,16 @@ public class UICoinController : MonoBehaviour
     {
         _coinCount++;
     }
+
+    private void CollectCoinEffect()
+    {
+        _coinText.fontSize = 24;
+        Invoke("ResetFontSize", 1);
+    }
+
+    private void ResetFontSize()
+    {
+        _coinText.fontSize = _defaultFontSize;
+    }
+
 }
