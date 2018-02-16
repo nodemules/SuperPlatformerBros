6c8534491e01af43e44f7a62bde9cead16188e3e
staticsound <johngreedjr@gmail.com>
2018-02-15 21:55:21 -0500

    2D Side Scroller (Super Platformer Bros) Episode 2.9999996 - Lives UI
    * Create a UI for showing number of Lives

---
+++ Assets/Scripts/UILivesController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class UILivesController : MonoBehaviour
+    {
+        private Text _lifeText;
+        
+        public void Start()
+        {
+            _lifeText = GetComponentInChildren<Text>();
+        }
+
+        public void Update()
+        {
+            _lifeText.text = "x " + GlobalGameState.Lives;
+        }
+
+    }
+}
