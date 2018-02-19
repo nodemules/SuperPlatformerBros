bfbe48c4c2287a93286860c07c0baa2e21fa2d98
staticsound <johngreedjr@gmail.com>
2018-02-19 13:20:41 -0500

    #10 Make player attack based on user input
    * Basic proof of concept of this
    * currently left control is the attack button

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -1,10 +1,32 @@
-﻿using Enemy;
+﻿using System.Runtime.Serialization.Formatters;
+using Enemy;
 using UnityEngine;
 
 namespace Player
 {
     public class PlayerAttack : MonoBehaviour
     {
+        private BoxCollider2D _weaponBoxCollider2D;
+        private SpriteRenderer _weaponSpriteRenderer;
+        
+        private void Start()
+        {
+            
+            _weaponBoxCollider2D = gameObject.GetComponent<BoxCollider2D>();
+            _weaponBoxCollider2D.enabled = false;
+            _weaponSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            _weaponSpriteRenderer.enabled = false;
+
+        }
+
+        private void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Attack();
+            }
+        }
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             Enemy.Enemy enemy = other.gameObject.GetComponent<IEnemy>() as Enemy.Enemy;
@@ -13,5 +35,20 @@ namespace Player
                 enemy.Kill();
             }
         }
+
+        private void Attack()
+        {
+            _weaponBoxCollider2D.enabled = true;
+            _weaponSpriteRenderer.enabled = true;
+            
+            Invoke("ResetAttack", 1);
+        }
+        
+        private void ResetAttack()
+        {
+            _weaponBoxCollider2D.enabled = false;
+            _weaponSpriteRenderer.enabled = false;
+        }
+        
     }
 }
