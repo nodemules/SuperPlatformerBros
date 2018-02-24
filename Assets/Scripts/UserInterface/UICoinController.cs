3ee9b26be879c9dd7eb38dd2f7b57498dcc48431
brenthaertlein <brenthaertlein@gmail.com>
2018-02-24 14:44:00 -0500

    #32 - UI Elements should scale with resolution
    * fix UI elements not scaling with resolution (canvas scalar)
    * fix in-game UI position
    * add GameCanvas to all levels
    * move UI element classes to UserInterface namespace

---
+++ Assets/Scripts/UserInterface/UICoinController.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq.Expressions;
+using Environment;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UserInterface
+{
+    public class UICoinController : MonoBehaviour
+    {
+        private static int _defaultFontSize = 18;
+        private Text _coinText;
+        private GameObject _coinIcon;
+        private int _oldCoinCount;
+
+        public void Start()
+        {
+            _coinText = GetComponentInChildren<Text>();
+            _coinIcon = GameObject.Find("UICoinIcon");
+            _defaultFontSize = _coinText.fontSize;
+            _oldCoinCount = GlobalGameState.Coins;
+        }
+
+        private void Update()
+        {
+            if (_oldCoinCount < GlobalGameState.Coins)
+            {
+                CollectCoinEffect();
+            }
+
+            _oldCoinCount = GlobalGameState.Coins;
+            _coinText.text = "x " + GlobalGameState.Coins;
+            _coinIcon.transform.Rotate(new Vector2(0.0f, 45) * Time.deltaTime);
+        }
+
+        private void CollectCoinEffect()
+        {
+            _coinText.fontSize = 24;
+            Invoke("ResetFontSize", 1);
+        }
+
+        private void ResetFontSize()
+        {
+            _coinText.fontSize = _defaultFontSize;
+        }
+    }
+}
--- Assets/Scripts/UserInterface/UILivesController.cs
+++ Assets/Scripts/UserInterface/UILivesController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-namespace Assets.Scripts
+namespace UserInterface
 {
     public class UILivesController : MonoBehaviour
     {
