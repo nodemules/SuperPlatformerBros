018e3f6487df54d0add2a738c741b8fb537f6235
staticsound <johngreedjr@gmail.com>
2018-03-03 11:50:05 -0500

    #45 Level 7
    * Falling spikes because it just wasnt quite hard enough

---
+++ Assets/Scripts/Environment/FallingObject.cs
@@ -0,0 +1,21 @@
+﻿using Interfaces;
+using UnityEngine;
+
+namespace TriggerArea
+{
+    public class FallingObject : MonoBehaviour , ITriggerable
+    {
+        private Rigidbody2D _rigidbody2D;
+        private void Start()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        public void Trigger()
+        {
+            _rigidbody2D.gravityScale = 1;
+            Destroy(gameObject, 2f);
+        }
+
+    }
+}
