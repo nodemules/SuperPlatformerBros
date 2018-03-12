2b395ae4be9103d6688e95adcba45dd0fe415af8
staticsound <johngreedjr@gmail.com>
2018-03-12 19:10:28 -0400

    #59 Level 8
    * Skeleton of level

--- Assets/Scripts/Level/LevelLoader.cs
+++ Assets/Scripts/Level/LevelLoader.cs
@@ -9,7 +9,7 @@ namespace Level
     public class LevelLoader : MonoBehaviour
     {
         private static readonly string[] Levels =
-            {"Level1", "Level2", "Level3", "Level4", "Level5", "Level6", "Level7"};
+            {"Level1", "Level2", "Level3", "Level4", "Level5", "Level6", "Level7", "Level8"};
 
         private static readonly int HighestLevel = Levels.Length;
         public static readonly string FirstLevel = Levels[0];
