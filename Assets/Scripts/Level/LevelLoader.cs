54243ee5e3108f2b98faa9cb09a466bfca522f02
brenthaertlein <brenthaertlein@gmail.com>
2018-03-13 22:00:57 -0400

    #60 - Level 9
    * level 9
    * warp area

--- Assets/Scripts/Level/LevelLoader.cs
+++ Assets/Scripts/Level/LevelLoader.cs
@@ -9,7 +9,7 @@ namespace Level
     public class LevelLoader : MonoBehaviour
     {
         private static readonly string[] Levels =
-            {"Level1", "Level2", "Level3", "Level4", "Level5", "Level6", "Level7", "Level8"};
+            {"Level1", "Level2", "Level3", "Level4", "Level5", "Level6", "Level7", "Level8", "Level9"};
 
         private static readonly int HighestLevel = Levels.Length;
         public static readonly string FirstLevel = Levels[0];
---
+++ Assets/Scripts/TriggerArea/WarpArea.cs
@@ -0,0 +1,27 @@
+﻿using Interfaces;
+using PlayerCharacter;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TriggerArea
+{
+    public class WarpArea : MonoBehaviour, IBoundary, ITriggerable
+    {
+        public bool IsObstacle
+        {
+            get { return false; }
+        }
+
+        public void Trigger()
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            GameObject levelContainer = GameObject.Find(scene.name + "Container");
+            Player player = levelContainer.GetComponentInChildren<Player>();
+            if (player != null)
+            {
+                BoxCollider2D boxCollider2D = GetComponent<BoxCollider2D>();
+                player.transform.position = boxCollider2D.bounds.center;
+            }
+        }
+    }
+}
