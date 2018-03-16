5547af26da8d83ac60aefb59dae7e8fb69de04e8
staticsound <johngreedjr@gmail.com>
2018-03-16 17:12:52 -0400

    #63 Level 10
    * Boss Prefab

---
+++ Assets/Scripts/Foe/ComplexWalkerEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Interfaces;
+using PlayerCharacter;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Foe
+{
+    public class ComplexWalkerEnemy : SimpleWalkerEnemy, IBoss
+    {
+        public Transform Player;
+        public float SmoothTime = 5.0f;
+        
+        private Vector3 _smoothVelocity = Vector3.zero;
+
+        public new void Update()
+        {
+            base.Update();
+            Move();
+        }
+
+        protected override void Move()
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, Player.position,
+                ref _smoothVelocity, SmoothTime);
+            
+        }
+    }
+}
