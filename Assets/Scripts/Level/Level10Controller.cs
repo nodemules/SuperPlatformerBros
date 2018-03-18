ba2d702b42ab3e7db7ec897180ccdf8ca8dbc4bc
staticsound <johngreedjr@gmail.com>
2018-03-18 16:00:36 -0400

    #63 Level 10
    * Opening speech

--- Assets/Scripts/Level/Level10Controller.cs
+++ Assets/Scripts/Level/Level10Controller.cs
@@ -4,12 +4,18 @@ using Foe;
 using PlayerCharacter;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Level
 {
     public class Level10Controller : MonoBehaviour
     {
         private const string SceneName = "Level10";
+        public Text UIText;
+        public string TextToType;
+        public float TimeToType;
+
+        private float textPercentage;
         private List<IBoss> _bosses = new List<IBoss>();
         private bool _bossesCleared;
         private Scene _scene;
@@ -19,7 +25,6 @@ namespace Level
         public void Start()
         {
             _scene = SceneManager.GetActiveScene();
-
             Invoke("StartFight", 20);
         }
 
@@ -29,6 +34,8 @@ namespace Level
             {
                 return;
             }
+
+            IntroSpeech();
 
             _bosses = new List<IBoss>(GetComponentsInChildren<IBoss>());
             bool lastBoss = _bosses.Count == 1;
@@ -41,12 +48,21 @@ namespace Level
                     boss.PowerUp();
                 }
             }
+
             _bossesCleared = _bosses.Count == 0;
 
             if (_bossesCleared)
             {
                 Invoke("WinGame", 3);
             }
+        }
+
+        private void IntroSpeech()
+        {
+            int numberOfLettersToShow = (int) (TextToType.Length * textPercentage);
+            UIText.text = TextToType.Substring(0, numberOfLettersToShow);
+            textPercentage += Time.deltaTime / TimeToType;
+            textPercentage = Mathf.Min(1.0f, textPercentage);
         }
 
         private void StartFight()
@@ -56,16 +72,22 @@ namespace Level
             {
                 boss.StartMoving();
             }
-           
+
             PlayerMovement movement = Player.GetComponent<PlayerMovement>();
             movement.MovementEnabled = true;
-
+            
+            Invoke("RemoveText", 2);
         }
 
         private void WinGame()
         {
             ApplicationState.Ending = -100;
             SceneManager.LoadScene("GameOver");
+        }
+
+        private void RemoveText()
+        {
+            UIText.enabled = false;
         }
     }
 }
