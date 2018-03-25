13f16bc3bcb543ad8a40858d574c9e5fb1dd41ae
brenthaertlein <brenthaertlein@gmail.com>
2018-03-24 21:04:26 -0400

    #58 - Short cooldown on Marsh's attack
    * prevent player from attacking while attack animation is occurring

--- Assets/Scripts/PlayerCharacter/PlayerAttack.cs
+++ Assets/Scripts/PlayerCharacter/PlayerAttack.cs
@@ -6,14 +6,17 @@ namespace PlayerCharacter
 {
     public class PlayerAttack : MonoBehaviour
     {
+        public float AttackDuration = 1.0f;
+        public AudioClip AttackAudio;
+        
         private BoxCollider2D _weaponBoxCollider2D;
         private SpriteRenderer _weaponSpriteRenderer;
         private Animator _weaponAnimator;
-        public AudioClip AttackAudio;
         private AudioSource _attackAudioSource;
 
         private Player _player;
-        
+        private bool _isAttacking;
+
         private void Start()
         {
             Initialize();
@@ -42,20 +45,23 @@ namespace PlayerCharacter
 
         private void Attack()
         {
-            if (_player.IsDead)
+            if (_player.IsDead || _isAttacking)
             {
                 return;
             }
+
+            _isAttacking = true;
             _attackAudioSource.PlayOneShot(AttackAudio);
             _weaponBoxCollider2D.enabled = true;
             _weaponSpriteRenderer.enabled = true;
             _weaponAnimator.enabled = true;   
             
-            Invoke("ResetAttack", 1);
+            Invoke("ResetAttack", AttackDuration);
         }
         
         private void ResetAttack()
         {
+            _isAttacking = false;
             _weaponBoxCollider2D.enabled = false;
             _weaponSpriteRenderer.enabled = false;
             _weaponAnimator.enabled = false;
