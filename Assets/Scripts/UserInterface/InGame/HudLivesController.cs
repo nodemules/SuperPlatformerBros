537cd345fd34019caba733a4f81e6b8ac9799bdb
staticsound <johngreedjr@gmail.com>
2018-04-08 20:27:32 -0400

    #14 Gain extra life from coins
    * Gain extra life with every 10 coins collected

--- Assets/Scripts/UserInterface/InGame/HudLivesController.cs
+++ Assets/Scripts/UserInterface/InGame/HudLivesController.cs
@@ -7,15 +7,35 @@ namespace UserInterface.InGame
     public class HudLivesController : MonoBehaviour
     {
         private Text _lifeText;
+        private int _oldLifeCount;
+        private static int _defaultFontSize = 18;
         
         public void Start()
         {
             _lifeText = GetComponentInChildren<Text>();
+            _oldLifeCount = GlobalGameState.Lives;
         }
 
         public void Update()
         {
+            if (_oldLifeCount < GlobalGameState.Lives)
+            {
+                ExtraLifeEffect();
+            }
+            
+            _oldLifeCount = GlobalGameState.Lives;
             _lifeText.text = "x " + GlobalGameState.Lives;
+        }
+        
+        private void ExtraLifeEffect()
+        {
+            _lifeText.fontSize = 24;
+            Invoke("ResetFontSize", 1);
+        }
+
+        private void ResetFontSize()
+        {
+            _lifeText.fontSize = _defaultFontSize;
         }
 
     }
