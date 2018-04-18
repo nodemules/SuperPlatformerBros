0c43b485c5b541cd6d7718ef9c31809292c70561
staticsound <johngreedjr@gmail.com>
2018-04-17 21:09:56 -0400

    #91 #94 #95 Hack a fix for this dumb TMP and Canvas prefab issue
    * create script to brute force elements into place no matter where they are dancing around to on the editor screen

---
+++ Assets/Scripts/UserInterface/InGame/HudHackTextPosition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UserInterface.InGame
+{
+    public class HudHackTextPosition : MonoBehaviour
+    {
+        private RectTransform _rectTransform;
+        public float posX;
+        public float posY;
+
+        private void Start()
+        {
+            _rectTransform = gameObject.GetComponent<RectTransform>();
+            _rectTransform.anchoredPosition = new Vector2(posX, posY);
+        }
+    }
+}
